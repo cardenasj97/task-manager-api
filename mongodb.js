38964ac207dbb6651a6af5d9785b1c82c5f99e0e
Juan Cardenas <cardenas19890@gmail.com>
2019-11-18 09:00:44 -0400

    CRUD developed and routers added

--- mongodb.js
+++ mongodb.js
@@ -11,24 +11,4 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
     }
 
     const db = client.db(databaseName)
-
-    // db.collection('users').findOne({ _id: new ObjectID('5dc87695cc4143189855034f') }, (error, user) => {
-    //     if (error) {
-    //         return console.log('Unable to fetch')
-    //     }
-
-    //     console.log(user)
-    // })
-
-    // db.collection('users').find({ age: 50 }).toArray((error, users) => {       
-    //     console.log(users)
-    // })
-
-    db.collection('tasks').findOne({ _id: new ObjectID("5dc874b59da0f51f886f9180") }, (error, task) => {
-        console.log(task)        
-    })
-
-    db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
-        console.log(tasks)
-    })
 })
