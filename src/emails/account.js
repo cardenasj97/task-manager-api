b7e52ef740ab56e5a6d07fd0e970e043bfae57fd
unknown <cardenas19890@gmail.com>
2019-12-08 21:33:18 -0400

    Init commit

--- src/emails/account.js
+++ src/emails/account.js
@@ -5,7 +5,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'cardenas19890@gmail.com',
+        from: 'john@example.com',
         subject: 'Welcome to the App',
         text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
     })
@@ -14,7 +14,7 @@ const sendWelcomeEmail = (email, name) => {
 const sendCancelationEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'cardenas19890@gmail.com',
+        from: 'john@example.com',
         subject: 'Sorry to see you go!',
         text: `Goodbye, ${name}. We hope to see you back soon.`
     })
