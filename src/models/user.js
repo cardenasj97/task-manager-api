fa7473f4b35c65d7f34eda38d69d8ca96b897bfe
unknown <cardenas19890@gmail.com>
2019-12-22 09:30:12 -0400

    december 22 update

--- src/models/user.js
+++ src/models/user.js
@@ -72,7 +72,6 @@ userSchema.methods.toJSON = function () {
     return userObject
 }
 
-
 userSchema.methods.generateAuthToken = async function () {
     const user = this
     const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET)
